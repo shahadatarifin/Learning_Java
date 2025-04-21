20e1aa2f57efaf3c9d16f1f6dc6d305f299b225e
Shahadat Arifn <149145008+shahadatarifin@users.noreply.github.com>
2025-04-21 23:57:19 +0530

    update

--- Java/Harry/practice set/Chapter-1/Sol_1.java
+++
@@ -1,7 +0,0 @@
-public class Sol_1 {
-    public static void main(String[] args){
-        int n1 = 34, n2 = 36, n3 = 98;
-        int sum = n1 + n2 + n3;
-        System.out.println(sum);
-    }
-}
--- Java/Harry/practice set/Chapter-1/Sol_4.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Sol_4 {
-    public static void main(String[] args) {
-        System.out.print("Enter a number: ");
-        Scanner n = new Scanner(System.in);
-        System.out.println(n.hasNextInt());
-    }
-}

de9bf534b86c5838e4c1395eb2ed9b11daf03984
Shahadat Arifn <149145008+shahadatarifin@users.noreply.github.com>
2025-05-13 14:58:22 +0600

    update

--- Java/Harry/Tutorial/X_10031_Method.java
+++ Java/Harry/Tutorial/X_10031_Method.java
@@ -1,9 +1,6 @@
 public class X_10031_Method {
 
-    static void tellJoke(){
-        System.out.println("One joke, coming up! What is a sea monster’s favorite snack? Ships and dip.");
-    }
-    static int logic(int x, int y){
+    static int staticMethod(int x, int y){
         if (x > y){
             return x;
         } else {
@@ -11,12 +8,29 @@ public class X_10031_Method {
         }
     }
 
+    int instanceMethod(int x, int y){
+        if (x > y){
+            return x;
+        } else {
+            return y;
+        }
+    }
+
+    static void tellJoke(){
+        System.out.println("One joke, coming up! What is a sea monster’s favorite snack? Ships and dip.");
+    }
+
     public static void main(String[] args) {
-        int a = 23;
-        int b = 45;
-        int c = logic(a, b);
-        System.out.println("largest number -> " + c);
-        System.out.println("joke -> ");
+        int a = 24;
+        int b = 44;
+        int c1 = staticMethod(a, b);
+        System.out.println("Largest number -> " + c1);
+
+        X_10031_Method obj = new X_10031_Method();
+        int c2 = obj.instanceMethod(a, b);
+        System.out.println("Largest number -> " + c2);
+
+        System.out.print("Joke -> ");
         tellJoke();
     }
 }

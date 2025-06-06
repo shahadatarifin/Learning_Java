0e45d4bdff62efac1f31ea5f9e283ecbcba7d19c
Shahadat Arifn <149145008+shahadatarifin@users.noreply.github.com>
2025-06-06 17:00:23 +0600

    update

---
+++ Java/Harry/practice set/Chapter_8.java
@@ -0,0 +1,81 @@
+public class Chapter_8 {
+/*
+1.  Create a class employee with following properties and methods
+    Salary(property)(int)
+    getSalary(method returning int)
+    name(property)(String)
+    getName(method returning String)
+    setName(method changing name)
+
+2. Create a class cellphone with methods to print "ringint...", "vigrating...", etc
+ */
+}
+
+
+
+
+// Solution 1
+class Sol_8_1{
+    static class Employee{
+        int salary;
+        String name;
+
+        public int getSalary(){
+            return salary;
+        }
+        public String getName(){
+            return name;
+        }
+        public void setName(String n){
+            name = n;
+        }
+    }
+
+    public static void main(String[] args) {
+        Employee shahadat = new Employee();
+        shahadat.setName("Shahadat Arifin");
+        shahadat.salary = 20;
+        System.out.println(shahadat.getName());
+        System.out.println(shahadat.getSalary());
+    }
+}
+
+
+
+
+// Solution 2
+class Sol_8_2{
+    static class CellPhone{
+        public void ring(){
+            System.out.println("Ringing...");
+        }
+
+        public void vibrate(){
+            System.out.println("Vigrating...");
+        }
+    }
+
+    public static void main(String[] args) {
+        CellPhone ArifinsIphone = new CellPhone();
+        ArifinsIphone.ring();
+        ArifinsIphone.vibrate();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
